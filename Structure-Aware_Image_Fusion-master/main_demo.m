754188379745c0f79d5f7b054f5c698e1cdf1ca5
Arman Gorji <gorjiarman@gmail.com>
2022-07-29 15:03:16 +0430

    Structure aware up and running

--- Structure-Aware_Image_Fusion-master/main_demo.m
+++ Structure-Aware_Image_Fusion-master/main_demo.m
@@ -1,14 +1,21 @@
 clear;
 close all
 addpath(genpath(pwd));
-for i=1:10
-    I1 = imread(['../ir/',num2str(i),'.bmp']);
-    I2 = imread(['../vis/',num2str(i),'.bmp']);
-    J(:,:,1) = I1;
-    J(:,:,2) = I2;
-    F_echo_dtf = IJF(I1,I2);
-    imwrite(F_echo_dtf,[num2str(i),'.bmp']);
-    clear J;
+V = niftiread('./HANCT.nii');
+[ri,ci,si] = size(V);
+disp(si)
+disp(ci)
+disp(ri)
+P = niftiread('./HANPT.nii');
+for s = 1:si
+V1 = double(squeeze(V(:,:,s)));
+P1 = double(squeeze(P(:,:,s)));
+    J(:,:,1) = V1;
+    J(:,:,2) = P1;
+    F_echo_dtf = IJF(V1,P1);
+    %imwrite(F_echo_dtf,[num2str(i),'.bmp']);
+T(:,:,s) = F_echo_dtf;
+clear J;
 end
-
-Q_echo_dtf = Qp_ABF(I1, I2, F_echo_dtf)
+niftiwrite(T,'outbrain.nii');
+Q_echo_dtf = Qp_ABF(V1, P1, F_echo_dtf)
