30ac67c7f4aa7d11053d299a5f3ec3e6163cabd9
Arman Gorji <gorjiarman@gmail.com>
2022-08-02 20:18:28 +0430

    Working with sample nifty ✅

--- Structure-Aware_Image_Fusion-master/main_demo.m
+++ Structure-Aware_Image_Fusion-master/main_demo.m
@@ -3,19 +3,17 @@ close all
 addpath(genpath(pwd));
 V = niftiread('./HANCT.nii');
 [ri,ci,si] = size(V);
-disp(si)
-disp(ci)
-disp(ri)
 P = niftiread('./HANPT.nii');
 for s = 1:si
-V1 = double(squeeze(V(:,:,s)));
-P1 = double(squeeze(P(:,:,s)));
-    J(:,:,1) = V1;
-    J(:,:,2) = P1;
+V1 = (double(squeeze(V(:,:,s)))/1000)*255;
+P1 = (double(squeeze(P(:,:,s)))/4)*255;
+%     J(:,:,1) = V1;
+%     J(:,:,2) = P1;
     F_echo_dtf = IJF(V1,P1);
     %imwrite(F_echo_dtf,[num2str(i),'.bmp']);
+    
 T(:,:,s) = F_echo_dtf;
 clear J;
 end
 niftiwrite(T,'outbrain.nii');
-Q_echo_dtf = Qp_ABF(V1, P1, F_echo_dtf)
+%Q_echo_dtf = Qp_ABF(V1, P1, F_echo_dtf)
