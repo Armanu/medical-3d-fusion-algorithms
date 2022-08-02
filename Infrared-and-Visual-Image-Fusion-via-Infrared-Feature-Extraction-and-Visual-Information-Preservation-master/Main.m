0a5b2bec35768e1d876b0acf9937e6d53d59c5b5
Arman Gorji <gorjiarman@gmail.com>
2022-08-02 17:29:34 +0430

    Working with sample nifty ✅

--- Infrared-and-Visual-Image-Fusion-via-Infrared-Feature-Extraction-and-Visual-Information-Preservation-master/Main.m
+++ Infrared-and-Visual-Image-Fusion-via-Infrared-Feature-Extraction-and-Visual-Information-Preservation-master/Main.m
@@ -14,17 +14,24 @@ MaxRatio = 0.001;
 StdRatio = 0.8;
 
 % image sets
-names = {'Camp', 'Camp1', 'Dune', 'Gun', 'Navi', 'Kayak', 'Octec', 'Road', 'Road2' 'Steamboat', 'T2', 'T3', 'Trees4906', 'Trees4917'};
+% names = {'Camp', 'Camp1', 'Dune', 'Gun', 'Navi', 'Kayak', 'Octec', 'Road', 'Road2' 'Steamboat', 'T2', 'T3', 'Trees4906', 'Trees4917'};
 
+V = niftiread('./HANCT.nii');
+P = niftiread('./HANPT.nii');
+[ri,ci,si] = size(V);
+for s = 1:si
+V1 = uint8((double(squeeze(V(:,:,s)))/1000)*255);
+P1 = uint8((double(squeeze(P(:,:,s)))/4)*255);
 % read one image set
-a = 10;
-setName = num2str(a);
-imgVis = imread(strcat('vis\', setName, '.bmp'));
-imgIR = imread(strcat('ir\', setName, '.bmp'));
+% a = 10;
+% setName = num2str(a);
+% imgVis = imread(strcat('vis\', setName, '.bmp'));
+% imgIR = imread(strcat('ir\', setName, '.bmp'));
 
 % image fusion
-result = BGR_Fuse(imgVis, imgIR, QuadNormDim, QuadMinDim, GaussScale, MaxRatio, StdRatio);
+result = BGR_Fuse(V1, P1, QuadNormDim, QuadMinDim, GaussScale, MaxRatio, StdRatio);
 
 % show image
-
-imwrite(result, strcat(setName, '.bmp'));
+T(:,:,s) = result;
+end
+niftiwrite(T,'outbrain.nii');
