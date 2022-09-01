48607a67b0c864e5cf260802879ad5635f712463
Arman Gorji <gorjiarman@gmail.com>
2022-09-01 10:58:18 +0430

    ready to run on workstation

--- CONVSR/CSR_Fusion_Author/Script.m
+++ CONVSR/CSR_Fusion_Author/Script.m
@@ -1,90 +1,66 @@
-
-% Load dictionary
-% load('dict.mat');  
-% n = 20;
-% time = zeros(n,1);
-% 
-% label='';
-% for k = 1:5
-% if k==1
-%     label='_gau_001';
-% end
-% if k==2
-%     label='_gau_0005';
-% end
-% if k==3
-%     label='_sp_01';
-% end
-% if k==4
-%     label='_sp_02';
-% end
-% if k==5
-%     label='_poi';
-% end
-% disp(label);
-
 %%----------------START OF NIFTY FOLDER LOOP-----------------%%
 %%-----------------------------------------------------------%%
 %%-----------------------------------------------------------%%
 %%-----------------------------------------------------------%%
 %%-----------------------------------------------------------%%
 
-ctfilelist = dir(fullfile('../CT_images/', '*.nii'));
-petfilelist = dir(fullfile('../PET_images/', '*.nii'));
+ctfilelist = dir(fullfile('C:\Users\Administrator\Downloads\CT\CT\', '*.nii.gz'));
 
-for ctfile = ctfilelist
-V = niftiread('../CT_images/'+string(ctfile.name));
-P = niftiread('../PET_images/'+string(ctfile.name));
-PF = P(:);
-PMA = max(PF);
-PMI = min(PF);
-VF = V(:);
-VMA = max(VF);
-VMI = min(VF);
-[ri,ci,si] = size(V);
-for s = 1:si
-V1 = (double(squeeze(V(:,:,s)))/(VMA-VMI))*255;
-P1 = (double(squeeze(P(:,:,s)))/(PMA-PMI))*255;
+for i = 1:length(ctfilelist)
+    newfile = split(string(ctfilelist(i).name),'.');
+    newfile = string(newfile{1});
+    newfolder = 'C:\Users\Administrator\Documents\GitHub\medical-3d-fusion-algorithms\CONVSR\OUTPUT\'+newfile+'\';
+    if ~exist(newfolder, 'dir')
+        if exist('C:\Users\Administrator\Downloads\PET\PET\'+string(ctfilelist(i).name), 'file')
+            V = niftiread('C:\Users\Administrator\Downloads\CT\CT\'+string(ctfilelist(i).name));
+            P = niftiread('C:\Users\Administrator\Downloads\PET\PET\'+string(ctfilelist(i).name));
+            clear T;
 
-% path1 = ['./IV_images/IR',num2str(index),'.png'];
-% path2 = ['./IR_images/VIS',num2str(index),'.png'];
-% fused_path = ['./fused_infrared/fused',num2str(index),'_ConvSR.png'];
-
-% path1 = ['./MF_images/image',num2str(index),'_left.png'];
-% path2 = ['./MF_images/image',num2str(index),'_right.png'];
-% fused_path = ['./fused_mf/fused',num2str(index),'_ConvSR.png'];
-% 
-% path1 = ['../../_________________________DATA/mid/Test_ir/',num2str(i),'.bmp'];
-% path2 = ['../../_________________________DATA/mid/Test_ir/',num2str(i),'.bmp'];
-% fused_path = ['../..//3/',num2str(i),label,'_convsr.bmp'];
-
-% Load images
-% A=imread('sourceimages/s01_1.tif');
-% B=imread('sourceimages/s01_2.tif');
-A=V1;
-B=P1;
-
-% figure,imshow(A)
-% figure,imshow(B)
-
-%key parameters
-lambda=0.01; 
-flag=1; % 1 for multi-focus image fusion and otherwise for multi-modal image fusion
-
-%CSR-based fusion
-tic;
-F=CSR_Fusion(A,B,3,lambda,flag);
-
-toc;
-
-% % figure,imshow(F);
-% imwrite(F,fused_path);
-% 
-% end
-% end
-T(:,:,s) = F;
-end
-niftiwrite(T,'../OUTPUT/'+string(ctfile.name));
+            if ~exist(newfolder,'dir')
+                mkdir(newfolder)
+            end
+            
+            disp("file : "+ string(ctfile(i).name))
+            
+            
+            PF = P(:);
+            PMA = max(PF);
+            PMI = min(PF);
+            VF = V(:);
+            VMA = max(VF);
+            VMI = min(VF);
+            [ri,ci,si] = size(V);
+            P = imresize3(P,[ri,ci,si]);
+%             if exist('C:\Users\Administrator\Downloads\Labels\PET\'+string(ctfilelist(i).name), 'file')
+%                 PL = niftiread('C:\Users\Administrator\Downloads\Labels\PET\'+string(ctfilelist(i).name));
+%                 CL = niftiread('C:\Users\Administrator\Downloads\Labels\CT\'+string(ctfilelist(i).name));
+%                 PL =  imresize3(PL,[ri,ci,si]);
+%                 CL =  imresize3(CL,[ri,ci,si]);
+%                 niftiwrite(CL,newfolder+newfile+'ctlabel.nii');
+%                 niftiwrite(PL,newfolder+newfile+'petlabel.nii');
+%             end
+            for s = 1:si
+                V1 = (double(squeeze(V(:,:,s)))/(VMA-VMI))*255;
+                P1 = (double(squeeze(P(:,:,s)))/(PMA-PMI))*255;
+                
+                A=V1;
+                B=P1;
+                
+                %key parameters
+                lambda=0.01;
+                flag=0; % 1 for multi-focus image fusion and otherwise for multi-modal image fusion
+                
+                %CSR-based fusion
+                F=CSR_Fusion(A,B,3,lambda,flag);
+                disp("slice : "+ string(s))
+                
+                T(:,:,s) = F;
+            end
+            
+            niftiwrite(T,newfolder+newfile+'.nii');
+            
+        end
+    end
 end
 %%-----------------------------------------------------------%%
 %%-----------------------------------------------------------%%
